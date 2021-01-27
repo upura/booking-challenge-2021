f70ba82972affba54d2d2fc5f8b0c679632fcd0e
upura <upura0@gmail.com>
2021-01-28 01:31:13 +0900

    Use Google Colab

--- access_colab.sh
+++ access_colab.sh
@@ -3,6 +3,6 @@
 for i in `seq 0 24`
 do
   echo "[$i]" ` date '+%y/%m/%d %H:%M:%S'` "connected."
-  open https://colab.research.google.com/drive/17aU9j3lpWTxi85qGl2IBwQN2dxzvYPQr?authuser=1
+  open https://colab.research.google.com/drive/1iewPD9nKSa1bl-fywGfIBGlJQQalpJsj?authuser=1
   sleep 3600
 done
