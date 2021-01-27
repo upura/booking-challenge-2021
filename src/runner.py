0443d740388c9eec70a4ffc704388ae786cce80e
upura <upura0@gmail.com>
2021-01-28 01:44:29 +0900

    Add multi-task learning models

--- src/runner.py
+++ src/runner.py
@@ -101,3 +101,106 @@ class CustomRunner(Runner):
             num_stay_consecutively_tensor,
         )
         return out
+
+
+class CustomRunnerMtl(Runner):
+    def _handle_batch(self, batch):
+        (
+            city_id_tensor,
+            booker_country_tensor,
+            device_class_tensor,
+            affiliate_id_tensor,
+            month_checkin_tensor,
+            num_checkin_tensor,
+            days_stay_tensor,
+            days_move_tensor,
+            hotel_country_tensor,
+            num_visit_drop_duplicates_tensor,
+            num_visit_tensor,
+            num_visit_same_city_tensor,
+            num_stay_consecutively_tensor,
+            y_s,
+            y_h,
+        ) = batch
+        out_s, out_h = self.model(
+            city_id_tensor,
+            booker_country_tensor,
+            device_class_tensor,
+            affiliate_id_tensor,
+            month_checkin_tensor,
+            num_checkin_tensor,
+            days_stay_tensor,
+            days_move_tensor,
+            hotel_country_tensor,
+            num_visit_drop_duplicates_tensor,
+            num_visit_tensor,
+            num_visit_same_city_tensor,
+            num_stay_consecutively_tensor,
+        )
+        loss_s = self.criterion(out_s, y_s)
+        loss_h = self.criterion(out_h, y_h)
+        loss = loss_s * 0.9 + loss_h * 0.1
+        accuracy01, accuracy04 = metrics.accuracy(out_s, y_s, topk=(1, 4))
+        self.batch_metrics.update(
+            {"loss": loss, "accuracy01": accuracy01, "accuracy04": accuracy04}
+        )
+        if self.is_train_loader:
+            loss.backward()
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+
+    @torch.no_grad()
+    def predict_batch(self, batch):
+        batch = any2device(batch, self.device)
+        if len(batch) == 13:
+            (
+                city_id_tensor,
+                booker_country_tensor,
+                device_class_tensor,
+                affiliate_id_tensor,
+                month_checkin_tensor,
+                num_checkin_tensor,
+                days_stay_tensor,
+                days_move_tensor,
+                hotel_country_tensor,
+                num_visit_drop_duplicates_tensor,
+                num_visit_tensor,
+                num_visit_same_city_tensor,
+                num_stay_consecutively_tensor,
+            ) = batch
+        elif len(batch) == 15:
+            (
+                city_id_tensor,
+                booker_country_tensor,
+                device_class_tensor,
+                affiliate_id_tensor,
+                month_checkin_tensor,
+                num_checkin_tensor,
+                days_stay_tensor,
+                days_move_tensor,
+                hotel_country_tensor,
+                num_visit_drop_duplicates_tensor,
+                num_visit_tensor,
+                num_visit_same_city_tensor,
+                num_stay_consecutively_tensor,
+                y_s,
+                y_h,
+            ) = batch
+        else:
+            raise RuntimeError
+        out_s, out_h = self.model(
+            city_id_tensor,
+            booker_country_tensor,
+            device_class_tensor,
+            affiliate_id_tensor,
+            month_checkin_tensor,
+            num_checkin_tensor,
+            days_stay_tensor,
+            days_move_tensor,
+            hotel_country_tensor,
+            num_visit_drop_duplicates_tensor,
+            num_visit_tensor,
+            num_visit_same_city_tensor,
+            num_stay_consecutively_tensor,
+        )
+        return out_s, out_h
