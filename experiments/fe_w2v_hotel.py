6886f46b0047ecd811fc83a2c707a3aab677536d
upura <upura0@gmail.com>
2021-01-28 00:25:16 +0900

    w2v for hotel_country

--- experiments/fe_w2v_hotel.py
+++ experiments/fe_w2v_hotel.py
@@ -14,17 +14,17 @@ if __name__ == '__main__':
     warnings.filterwarnings('ignore')
 
     train_test = load_train_test()
-    target_le = preprocessing.LabelEncoder()
-    train_test['city_id'] = target_le.fit_transform(train_test['city_id'])
-    train_test['city_id'] = train_test['city_id'].astype(str)
+    hotel_le = preprocessing.LabelEncoder()
+    train_test['hotel_country'] = hotel_le.fit_transform(train_test['hotel_country'])
+    train_test['hotel_country'] = train_test['hotel_country'].astype(str)
 
     train = train_test[train_test['row_num'].isnull()]
     test = train_test[~train_test['row_num'].isnull()]
 
-    # Delete last city_id to avoid leakage
-    train_trips = train.groupby('utrip_id')['city_id'].apply(lambda x: x.values[:-1]).reset_index()
-    test_trips = test.groupby('utrip_id')['city_id'].apply(lambda x: x.values).reset_index()
-    trips = pd.concat([train_trips, test_trips], sort=False)
+    # Delete last hotel_country to avoid leakage
+    train_hotels = train.groupby('utrip_id')['hotel_country'].apply(lambda x: x.values[:-1]).reset_index()
+    test_hotels = test.groupby('utrip_id')['hotel_country'].apply(lambda x: x.values).reset_index()
+    hotels = pd.concat([train_hotels, test_hotels], sort=False)
 
     cores = multiprocessing.cpu_count()
     w2v_model = Word2Vec(min_count=1,
@@ -34,9 +34,9 @@ if __name__ == '__main__':
                          min_alpha=0.0007,
                          workers=cores - 1)
 
-    sentences = [list(ar) for ar in trips['city_id'].to_list()]
+    sentences = [list(ar) for ar in hotels['hotel_country'].to_list()]
     w2v_model.build_vocab(sentences)
-    w2v_model.train(sentences, total_examples=w2v_model.corpus_count, epochs=150)
+    w2v_model.train(sentences, total_examples=w2v_model.corpus_count, epochs=25)
     w2v_model.save("word2vec.model")
 
     data = np.array([w2v_model.wv[d] for d in w2v_model.wv.vocab.keys()])
